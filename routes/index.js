65c7aa97b643ac24fccf6892f5982b00ecff6c97
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-01-29 14:43:53 +0000

    Add API to render pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET child list page. */
+router.get('/children', function(req, res, next) {
+  res.render('childList', { title: 'Children' });
+});
+
+/* GET add child page. */
+router.get('/addChild', function(req, res, next) {
+  res.render('addChild', { title: 'New Child' });
+});
+
 module.exports = router;
