3e3dcab96d0105cd926ad8f4affb6c4b635b31ed
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-02-13 15:28:28 +0000

    Set month to selection

--- public/javascripts/children.js
+++ public/javascripts/children.js
@@ -10,33 +10,42 @@
     }
   );
 
-$(document).ready(
-    function() {        
-        /**
-         * Event handler for when the user attempts to register
-         */
-        $("#child-form").submit(function (event) {
-            event.preventDefault();
-            $.ajax({
-                type: 'POST',
-                url: '/child/addChild',
-                dataType: 'json',
-                data: {
-                    'child_fname': event.target.input-FirstName.value,
-                    'child_fname': event.target.input-Surname.value,
-                    // HERE
-                },
-                success: function(token){
-                    $(location).attr('href', '/feed' );
-                    // Redirect to a login page
-                },
-                error: function(errMsg) {
-                    swal(
-                        'Oops...',
-                        errMsg.responseJSON.body,
-                        'error'
-                    )
-                }
-            });
-        }); 
-    });
+//$(document).ready(
+////    function() {        
+////        /**
+////         * Event handler for when the user attempts to register
+////         */
+////        $("#child-form").submit(function (event) {
+////            event.preventDefault();
+////            $.ajax({
+////                type: 'POST',
+////                url: '/child/addChild',
+////                dataType: 'json',
+////                data: {
+////                    'child_fname': event.target.input-FirstName.value,
+////                    'child_fname': event.target.input-Surname.value,
+////                    var dob = 
+////                },
+////                success: function(token){
+////                    $(location).attr('href', '/feed' );
+////                    // Redirect to a login page
+////                },
+////                error: function(errMsg) {
+////                    swal(
+////                        'Oops...',
+////                        errMsg.responseJSON.body,
+////                        'error'
+////                    )
+////                }
+////            });
+////        }); 
+//    });
+
+$('#monthsDropdown').click(function(event){
+    var month = event.target.textContent;
+    $("#input-Month").val(month);
+});
+//
+//$(document).on('change', 'input.monthsDropdown', function() {
+//    console.log($(this).val());
+//});
