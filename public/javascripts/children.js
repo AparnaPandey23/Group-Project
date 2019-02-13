69f824111369750601e5350d0db738fea7d9f21a
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-02-13 15:40:45 +0000

    Rename input ids

--- public/javascripts/children.js
+++ public/javascripts/children.js
@@ -10,40 +10,45 @@
     }
   );
 
-//$(document).ready(
-////    function() {        
-////        /**
-////         * Event handler for when the user attempts to register
-////         */
-////        $("#child-form").submit(function (event) {
-////            event.preventDefault();
-////            $.ajax({
-////                type: 'POST',
-////                url: '/child/addChild',
-////                dataType: 'json',
-////                data: {
-////                    'child_fname': event.target.input-FirstName.value,
-////                    'child_fname': event.target.input-Surname.value,
-////                    var dob = 
-////                },
-////                success: function(token){
-////                    $(location).attr('href', '/feed' );
-////                    // Redirect to a login page
-////                },
-////                error: function(errMsg) {
-////                    swal(
-////                        'Oops...',
-////                        errMsg.responseJSON.body,
-////                        'error'
-////                    )
-////                }
-////            });
-////        }); 
-//    });
+$(document).ready(
+    function() {        
+        /**
+         * Event handler for when the user adds a new child
+         */
+        $("#child-form").submit(function (event) {
+            event.preventDefault();
+            var day = event.target.input-Day.value;
+            var month = event.target.input-Month.value;
+            var year = event.target.input-Year.value;
+            var dob = day + '/' + month + '/' + year;
+            $.ajax({
+                type: 'POST',
+                url: '/child/addChild',
+                dataType: 'json',
+                data: {
+                    'child_fname': event.target.input-FirstName.value,
+                    'child_fname': event.target.input-Surname.value,
+                    'dob': dob,
+                    'parName': event.target.input-Parname
+                },
+                success: function(token){
+                    $(location).attr('href', '/children' );
+                    // Redirect to a list of children
+                },
+                error: function(errMsg) {
+                    swal(
+                        'Oops...',
+                        errMsg.responseJSON.body,
+                        'error'
+                    )
+                }
+            });
+        }); 
+    });
 
 $('#monthsDropdown').click(function(event){
     var month = event.target.textContent;
-    $("#input-Month").val(month);
+    $("#inputMonth").val(month);
 });
 //
 //$(document).on('change', 'input.monthsDropdown', function() {
