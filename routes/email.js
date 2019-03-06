27af0180d624cc5c80ef59aff27397f25796213f
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-03-06 16:30:49 +0000

    Add sending options

--- routes/email.js
+++ routes/email.js
@@ -4,6 +4,14 @@ var creds = require('../creds');
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
 
+var options = {
+    auth: {
+      service: 'SendGrid',
+      api_user: creds.user,
+      api_key: creds.psw
+    }
+  }
+
 // Send email to address
 router.post('/send', function(req, res, next){
     res.json(creds);
