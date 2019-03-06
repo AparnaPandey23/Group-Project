7cb78f96c56e08ef87fbfa6834674b5dae81fb7b
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-03-06 16:48:06 +0000

    Create email object to send

--- routes/email.js
+++ routes/email.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var creds = require('../creds');
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
+var util = require('util');
 
 var options = {
     auth: {
@@ -11,10 +12,20 @@ var options = {
       api_key: creds.psw
     }
   }
+  var client = nodemailer.createTransport(sgTransport(options));
+
 
 // Send email to address
 router.post('/send', function(req, res, next){
-    res.json(creds);
+    
+    var email = {
+        from: creds.email,
+        to: req.body.recipient,
+        subject: req.body.subject,
+        text: req.body.text,
+        html: req.body.html
+      };
+    res.json(email);
 });
 
 module.exports = router;
