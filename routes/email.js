ad88e787324fcfabbd18d4bead02df471c79993a
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-03-06 16:21:34 +0000

    Add POST request outline

--- routes/email.js
+++ routes/email.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Send email to address
+router.post('/sendEmail', function(req, res, next){
+    
+});
+
 module.exports = router;
