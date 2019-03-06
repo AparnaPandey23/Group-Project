023335dfc7590029702d89435782935cf6d43a81
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-03-06 18:09:14 +0000

    Add basic email validity checks

--- public/javascripts/email.js
+++ public/javascripts/email.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
     $('#forgotPswEmailBtn').click(function() {
         var emailRequest = constructEmailRequest();
-        sendEmail(emailRequest);
+        if(emailRequest) sendEmail(emailRequest);
     });
 });
 
 function constructEmailRequest(){
-    var emailRequest;
-    // Construct
+    var email = $('#emailInput').val();
+    if (validEmail(email))
+        var emailRequest = email;
+    else 
+        return null;
+    
     return emailRequest;
 }
 
 function sendEmail(emailRequest){
     console.log(emailRequest);
+}
+
+function validEmail(email){
+    // Basic checks so far
+    if (email.length == 0) return false;
+    else if (email.search('@') == -1) return false;
+    return true;
 }
