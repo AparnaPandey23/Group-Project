d394663d1393e3c2c5955841a3dba071e4496ba3
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-03-06 18:25:34 +0000

    Parts of email are added in route according to email type

--- routes/email.js
+++ routes/email.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var creds = require('../creds');
+var emailStructure = require('../emailStructure');
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
 
@@ -16,22 +17,29 @@ var options = {
 
 // Send email to caddress
 router.post('/send', function(req, res, next){
+    var subject, text, html;
+    if(req.body.type == "forgot-psw"){
+        subject = emailStructure.forgotPsw.subject;
+        text = emailStructure.forgotPsw.text;
+        html = emailStructure.forgotPsw.html;
+    }
     var email = {
         from: creds.email,
         to: req.body.recipient,
-        subject: req.body.subject,
-        text: req.body.text,
-        html: req.body.html
+        subject: subject,
+        text: text,
+        html: html
       };
    
-      client.sendMail(email, function(err, info){
-        if (err){
-          res.send(err);
-        }
-        else {
-          res.json({"Message sent:":info.response});
-        }
-    });
+    //   client.sendMail(email, function(err, info){
+    //     if (err){
+    //       res.send(err);
+    //     }
+    //     else {
+    //       res.json({"Message sent:":info.response});
+    //     }
+    // });
+
 });
 
 module.exports = router;
