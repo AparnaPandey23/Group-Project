2d1e178c0f97dcb7a685c03303cdffdce4424c4b
ETHAN PADDEN(28-09-2018 12-36 <danu7_pe@danu7>
2019-03-12 19:09:08 +0000

    Bug fixes

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -1,49 +1,25 @@
-function cookieId(name){
-    var user = name;
-        $.ajax({
-                type: 'GET',
-                url: '/users/getId',
-                dataType: 'json',
-                data: {
-                    'user_name': user
-                    },
-                success: function(token){
-                },
-                        error: function(errMsg) {
-                    swal(
-                        'Oops...',
-                        errMsg.responseJSON.body,
-                        'error'
-                    )
-                }
-            });
-                    
-    }
-    
-
-
 $(document).ready(
-    function() {        
-        /**
-         * Event handler for when the user attempts to register
-         */
+    // REGISTRATION
+    function() {        // 1. User enters details and hits sign up button
         $("#reg-form").submit(function (event) {
             event.preventDefault();
             $.ajax({
                 type: 'POST',
                 url: '/users/register',
-                dataType: 'json',
+                dataType: 'json', // 2. The following data is sent to the server in a post request:
                 data: {
                     'user_name': event.target.inputUsername.value,
                     'password': event.target.inputPassword.value,
                     'email': event.target.inputEmail.value
                 },
                 success: function(token){
-                    // Recieve the token sent back from the server
-                    console.log(token);
-                    // cookieId(event.target.inputUsername.value);
-                    // $(location).attr('href', '/feed' );
-                    // Redirect to a login page
+                    // 12. If the user successfully signed up,
+                    // recieve the token sent back from the server.
+                    // The token is automatically saved as a cookie (res.cookie)
+                    // This will be included in every further request until it expires
+
+                    // Redirect the user to the feed page
+                    $(location).attr('href', '/feed' );
                 },
                 error: function(errMsg) {
                     swal(
@@ -56,49 +32,22 @@ $(document).ready(
         }); 
     });
 
-
-function cookieId(name){
-    var user = name;
-    $.ajax({
-        type: 'GET',
-        url: '/users/getId',
-        dataType: 'json',
-        data: {
-            'user_name': user
-        },
-        success: function(token){
-            swal(
-                'shit',
-                'it worked'
-            ) 
-        },
-        error: function(errMsg) {
-            swal(
-                'Oops...',
-                'error'
-            )
-        }
-    });
-
-}
-
-
 $(document).ready(
-    function() {       
+    // LOGIN
+    function() {        // 1. User enters details and hits login button
         $("#log-form").submit(function (event) {
             event.preventDefault();
             $.ajax({
                 type: 'POST',
                 url: '/users/login',
-                dataType: 'json',
-                data: {
+                dataType: 'json', // 2. The following data is sent to the server in a post request:
+                data: { 
                     'user_name': event.target.inputUsername.value,
                     'password': event.target.inputPassword.value
                 },
                 success: function(token){
-                    cookieId(event.target.inputUsername.value);
+                    // 12. Redirect user to feed page
                     setTimeout(function(){ $(location).attr('href', '/feed' );}, 3000);
-                    // Redirect to logged in page
                 },
                 error: function(errMsg) {
                     swal(
@@ -117,64 +66,15 @@ $(document).ready(
         $("#profile-dropdown > li:nth-child(2) a").click(signOut);
     });
 
+// Creates a cookie with the ID of the user
+function createIdCookie(username) {
+    // Send GET request
+}
 
+// ???
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
 }
-
-// AUTH COURSE ===
-function signUp() {
-    console.log("User signing up");
-    $.ajax({
-        type: 'POST',
-        url: '/users/signup',
-        dataType: 'json',
-        data: {
-            
-        },
-        success: function(token){
-            
-        },
-        error: function(errMsg) {
-            console.log("Error: " + errMsg);
-        }
-    });
-}
-function signIn() {
-    console.log("User signing in");
-    $.ajax({
-        type: 'POST',
-        url: '/users/signin',
-        dataType: 'json',
-        data: {
-            
-        },
-        success: function(token){
-            
-        },
-        error: function(errMsg) {
-            console.log("Error: " + errMsg);
-        }
-    });
-}
-function accessSecret() {
-    console.log("User accessing secret data");
-    $.ajax({
-        type: 'POST',
-        url: '/users/secret',
-        dataType: 'json',
-        data: {
-            
-        },
-        success: function(token){
-            
-        },
-        error: function(errMsg) {
-            console.log("Error: " + errMsg);
-        }
-    });
-}
-// ===
