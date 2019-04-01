77b217fb3edfb70ea800257360d861b8752dc32d
aparna2 <heyho@gmail.com>
2019-04-01 16:19:11 +0100

    Implemented sign-out for all pages

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -142,12 +142,15 @@ $("#togBtn").on('change', function() {
     }
 });
 
-// ???
+//added
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
 
 var iconHTML = '<i class="mdi-navigation-arrow-drop-down right"></i>';
